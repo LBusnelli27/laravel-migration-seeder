45ba4c74cb6c33bf6be6ffd224dfe1ea6f920def
Busna27 <lucabusnelli2001@gmail.com>
2022-05-13 16:15:33 +0200

    migration trains + last setup

---
+++ resources/views/train/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.main')
+
+@section('title-tab', 'Index')
+
+@section('main-content')
+    <div class="container-fluid">
+        <h1 class="text-center mb-5">Ecco tutti i treni disponibili</h1>
+        <div class="row p-3">
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
